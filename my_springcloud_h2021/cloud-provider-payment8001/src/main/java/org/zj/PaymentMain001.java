aec1bceed16048795f79c8018a6bd59ff2b7be5f
周杰 <597690494@qq.com>
2021-06-15 17:23:12 +0800

    spring boot  支付消费模块加入eureka

--- my_springcloud_h2021/cloud-provider-payment8001/src/main/java/org/zj/PaymentMain001.java
+++ my_springcloud_h2021/cloud-provider-payment8001/src/main/java/org/zj/PaymentMain001.java
@@ -2,8 +2,10 @@ package org.zj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient//<-----添加该注解,加入到eureka 提供服务
 public class PaymentMain001 {
 
     public static void main(String[] args) {
