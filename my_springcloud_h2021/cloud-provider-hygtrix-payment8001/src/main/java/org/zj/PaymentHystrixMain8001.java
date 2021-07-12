9a8e4945c6ed09e17f69f385fcce2009f0695415
周杰 <597690494@qq.com>
2021-07-12 20:45:54 +0800

    64_Hystrix图形化Dashboard监控实战

--- my_springcloud_h2021/cloud-provider-hygtrix-payment8001/src/main/java/org/zj/PaymentHystrixMain8001.java
+++ my_springcloud_h2021/cloud-provider-hygtrix-payment8001/src/main/java/org/zj/PaymentHystrixMain8001.java
@@ -1,9 +1,14 @@
 package org.zj;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.Resource;
 
 /**
  * @author jiezhou
@@ -18,7 +23,28 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentHystrixMain8001.class,args);
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
 
+    }
+
+
+    /**
+     *此配置是为了服务监控而配置，与服务容错本身无关，springcloud升级后的坑
+     *ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
+     *只要在自己的项目里配置上下面的servlet就可以了
+     *否则，Unable to connect to Command Metric Stream 404
+     *
+     * ————————————————
+     *     版权声明：本文为CSDN博主「巨輪」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+     *     原文链接：https://blog.csdn.net/u011863024/article/details/114298282
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
