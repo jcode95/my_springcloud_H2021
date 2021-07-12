3811f8fd0f5f425359117d90fd399d576b4c2de0
周杰 <597690494@qq.com>
2021-07-12 20:31:47 +0800

    Hystrix图形化Dashboard搭建

---
+++ my_springcloud_h2021/cloud-consumer-hystrix-dashboard9001/src/main/java/org/zj/HystrixDashboardMain9001.java
@@ -0,0 +1,13 @@
+package org.zj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard//仪表盘 开启注解
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
