1a7d00f54e67c970a0080ca08cdd8bce491ca14e
周杰 <597690494@qq.com>
2021-07-13 16:19:49 +0800

    73_GateWay的Filter

---
+++ my_springcloud_h2021/cloud-gateway-gateway-9527/src/main/java/org/zj/filter/MyLogGateWayFilter.java
@@ -0,0 +1,45 @@
+package org.zj.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author jiezhou
+ * @CalssName: MyLogGateWayFilter
+ * @Package org.zj.filter
+ * @Description: 自定义过滤器
+ * @date 2021/7/13/16:15
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    Logger log = LoggerFactory.getLogger(MyLogGateWayFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***********come in MyLogGateWayFilter:  " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
