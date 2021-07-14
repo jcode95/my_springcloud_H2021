106dc59ce1c9473044b2820bf18572d209ff11fa
周杰 <597690494@qq.com>
2021-07-14 13:15:56 +0800

    81_Bus动态刷新全局广播配置实现

--- my_springcloud_h2021/cloud-config-client-3366/src/main/java/org/zj/controller/ConfigClientController.java
+++ my_springcloud_h2021/cloud-config-client-3366/src/main/java/org/zj/controller/ConfigClientController.java
@@ -5,20 +5,21 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ */
 @RestController
 @RefreshScope
 public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo() {
-        return configInfo;
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
     }
-    @GetMapping("/abc")
-    public String getabc() {
-        return "abc";
-    }
+
 }
 
