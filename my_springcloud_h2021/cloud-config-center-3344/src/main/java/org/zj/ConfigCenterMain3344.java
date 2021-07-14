c38a61bdafa278fca9346afe08ff62b4b52a75ab
周杰 <597690494@qq.com>
2021-07-14 13:18:32 +0800

    82_Bus动态刷新定点通知

--- my_springcloud_h2021/cloud-config-center-3344/src/main/java/org/zj/ConfigCenterMain3344.java
+++ my_springcloud_h2021/cloud-config-center-3344/src/main/java/org/zj/ConfigCenterMain3344.java
@@ -12,3 +12,21 @@ public class ConfigCenterMain3344 {
     }
 }
 
+
+/*
+*82_Bus动态刷新定点通知
+不想全部通知，只想定点通知
+
+只通知3355
+不通知3366
+简单一句话 - 指定具体某一个实例生效而不是全部
+
+公式：http://localhost:3344/actuator/bus-refresh/{destination}
+
+/bus/refresh请求不再发送到具体的服务实例上，而是发给config server通过destination参数类指定需要更新配置的服务或实例
+
+案例
+
+我们这里以刷新运行在3355端口上的config-client（配置文件中设定的应用名称）为例，只通知3355，不通知3366
+curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355
+* */
