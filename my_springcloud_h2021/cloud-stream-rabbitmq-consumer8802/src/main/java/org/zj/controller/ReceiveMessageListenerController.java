4d9f6e6e8a47ab073cc358f5b29c2435d9dd9477
周杰 <597690494@qq.com>
2021-07-14 14:11:13 +0800

    88_Stream消息驱动之消费者

---
+++ my_springcloud_h2021/cloud-stream-rabbitmq-consumer8802/src/main/java/org/zj/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,35 @@
+package org.zj.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author jiezhou
+ * @CalssName: ReceiveMessageListenerController
+ * @Package org.zj.controller
+ * @Description:
+ * @date 2021/7/14/14:05
+ */
+@RestController
+@Slf4j
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者1号,----->接受到的消息: " + message.getPayload() + "\t  port: " + serverPort);
+    }
+
+
+}
