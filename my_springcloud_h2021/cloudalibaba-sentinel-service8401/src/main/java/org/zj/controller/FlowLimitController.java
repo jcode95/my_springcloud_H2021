e75f990cb8d1fdb764a94bdb1375a333249566dc
周杰 <597690494@qq.com>
2022-01-12 15:44:54 +0800

    113_Sentinel初始化监控

---
+++ my_springcloud_h2021/cloudalibaba-sentinel-service8401/src/main/java/org/zj/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package org.zj.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiezhou
+ * @CalssName: FlowLimitController
+ * @Package org.zj.controller
+ * @Description:
+ * @date 2022/1/12/15:43
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+}
